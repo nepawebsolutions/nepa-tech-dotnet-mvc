02b5031a42c760a5c3189f7109c9c3afd02c7b03
Joecracko <joecracko@gmail.com>
2017-11-16 09:37:03 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+﻿/// <binding BeforeBuild='sass-compile, jquery-copy, bootstrap-copy' />
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+var paths = {
+    wwwroot: './wwwroot/'
+}
+
+paths.css = paths.wwwroot + 'css/'
+paths.js = paths.wwwroot + 'js/'
+
+gulp.task('sass-compile', function () {
+    gulp.src('./assets/sass/master.scss')
+        .pipe(sass())
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('jquery-copy', function () {
+    gulp.src('./node_modules/jquery/dist/jquery.js')
+        .pipe(gulp.dest(paths.js));
+})
+
+gulp.task('bootstrap-copy', function () {
+    gulp.src('./node_modules/bootstrap/dist/js/bootstrap.js')
+        .pipe(gulp.dest(paths.js));
+    //gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
+    //    .pipe(gulp.dest(paths.css));
+})
+
+gulp.task('watch-sass', function () {
+    gulp.watch('./Content/*.scss', ['sass-compile']);
+});
