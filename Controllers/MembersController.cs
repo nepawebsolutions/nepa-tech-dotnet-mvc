603d1230582962aa01c6d615d8bdbece2fa15a1d
Lucas Johns <johnslw26@mounties.mansfield.edu>
2018-05-30 12:11:16 -0400

    Finished paginated members list, added a redirect to events nav link so it goes to meetup

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NEPATechDotnetCoreMVC.ViewModel;
 
 namespace NEPATechDotnetCoreMVC.Controllers
 {
     public class MembersController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(int? page)
         {
-            return View();
+            var dummyMembers = Enumerable.Range(1, 150).Select(x => "Android #" + x);
+            var pager = new Pager(dummyMembers.Count(), page);
+            Random random = new Random();
+
+            var viewModel = new MembersViewModel {
+
+                Members = dummyMembers.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize),
+                Pager = pager,
+                description = "decidedly descript description of describing a disciple of dionysus for doccumentation determination",
+                dateJoined = (random.Next(1, 12)) + "/" +(random.Next(1,28))+"/"+ (random.Next(1991, 2800))
+            };
+            return View(viewModel);
         }
     }
 }
