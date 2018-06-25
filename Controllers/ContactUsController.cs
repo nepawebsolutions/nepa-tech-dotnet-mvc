f0bb286dc4a19571da7f3b2d6de1806bbc6a8406
Lucas Johns <johnslw26@mounties.mansfield.edu>
2018-06-25 11:44:46 -0400

    Contact page is functional

--- Controllers/ContactUsController.cs
+++ Controllers/ContactUsController.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NEPATechDotnetCoreMVC.Data;
 using NEPATechDotnetCoreMVC.ViewModel;
 
 namespace NEPATechDotnetCoreMVC.Controllers
 {
     public class ContactUsController : Controller
     {
-        public IActionResult Index(ContactViewModel contactModel)
+        [BindProperty]
+        public string fname { get; set; }
+        [BindProperty]
+        public string lname { get; set; }
+        [BindProperty]
+        public string email { get; set; }
+        [BindProperty]
+        public string complaint { get; set; }
+
+        private ContactViewModel contactViewModel = new ContactViewModel();
+        private NEPATechDbContext _context;
+
+        public ContactUsController(NEPATechDbContext _context)
         {
-            var name = contactModel.fullName;
-            return View();
+            contactViewModel.isComplaintSent = false;
+            this._context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            if(Request.Method == "POST")
+            {
+                var ContactLogs = _context.ContactLogs;
+                ContactLogs.Add(new Models.ContactLog.ContactLog
+                {
+                    FName = fname,
+                    LName = lname,
+                    Email = email,
+                    Message = complaint
+                });
+
+                _context.SaveChanges();
+                contactViewModel.isComplaintSent = true;
+            }
+            return View(contactViewModel);
         }
         public IActionResult eventsRedirect()
         {
--- ViewModel/ContactViewModel.cs
+++ ViewModel/ContactViewModel.cs
@@ -7,8 +7,6 @@ namespace NEPATechDotnetCoreMVC.ViewModel
 {
     public class ContactViewModel
     {
-        public string fullName { get; set; }
-        public string email { get; set; }
-        public string complaintMessage { get; set; }
+        public bool isComplaintSent { get; set; }
     }
 }
